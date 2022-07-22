4cdf3723d8ed249bf5b7c326d8e29240e7e3f975
QiluxanQ <ivan.ivan16@inbox.ru>
2022-07-22 15:13:52 +0300

    1-C

--- src/client/blocks/header/Header.tsx
+++ src/client/blocks/header/Header.tsx
@@ -2,23 +2,22 @@ import React, { FC } from 'react';
 import {IHeaderProps} from "./HeaderTypes";
 import styles from './Header.module.scss';
 
-const Header: FC<IHeaderProps> = ({ gender, age, socials }) => {
+const Header: FC<IHeaderProps> = ({ age, socials }) => {
 
   return (
     <div className={styles['header']}>
       <div className={styles['header-gender']}>
-        {`Hello ${gender}`}
       </div>
       <div>
         {age}
       </div>
-      <ul className={styles['header-socials']}>
+      <div className={styles['header-socials']}>
         {socials?.map(social => (
-            <li className={styles['header-social']}>
+            <a href="#" className={styles['header-social']}>
               {social}
-            </li>
+            </a>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
